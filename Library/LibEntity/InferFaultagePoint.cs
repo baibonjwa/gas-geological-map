a3159b7741574654fcfb37ca94c820a5b11dcf57
happybai <happybyronbai@gmail.com>
2015-05-19 14:50:28 +0800

    modified .gitignore

---
+++ Library/LibEntity/InferFaultagePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Castle.ActiveRecord;
+using NHibernate.Criterion;
+
+namespace LibEntity
+{
+    [ActiveRecord]
+    public class InferFaultagePoint : ActiveRecordBase<InferFaultagePoint>
+    {
+        [PrimaryKey(PrimaryKeyType.Identity, "id")]
+        public virtual int id { get; set; }
+
+        [Property]
+        public virtual string up_or_down { get; set; }
+
+        [Property]
+        public virtual double coordinate_x { get; set; }
+
+        [Property]
+        public virtual double coordinate_y { get; set; }
+
+        [Property]
+        public virtual double coordinate_z { get; set; }
+
+        [BelongsTo("id")]
+        public InferFaultage infer_faultage { get; set; }
+
+        [Property]
+        public virtual string bid { get; set; }
+    }
+}
